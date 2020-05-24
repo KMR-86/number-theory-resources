2d06963559f61c6c428e27db5da32f38c45993f4
khandaker mushfiqur rahman <1505086.kmr@ugrad.cse.buet.ac.bd>
2020-05-24 12:53:47 +0600

    the n-th fibonacchi number in log(n) time added

---
+++ codes/n-th_element_of_recurrence_relations/nth_element_of_recursion.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print_mat(long long int **a){
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+
+    }
+}
+long long int **sq_mat_mul(long long int **a, long long int **b,long long int n){
+
+    long long int **ans;
+    ans = new long long int *[n];
+    for(int i = 0; i <n; i++){
+        ans[i] = new long long int[n];
+    }
+
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            ans[i][j]=0;
+        }
+
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            for(int k=0;k<n;k++){
+                ans[i][j]+=a[i][k]*b[k][j];
+                //cout<<a[j][k]<<" "<<b[k][j]<<"  ";
+            }
+
+            //cout<<ans[i][j]<<" ";
+        }
+
+    }
+    //print_mat(ans);
+    return ans;
+
+}
+long long int **mat_power(long long int **mat,long long int p,long long int n){
+    long long int **t;
+
+    t= new long long int *[n];
+    for(int i = 0; i <n; i++){
+        t[i] = new long long int[n];
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j){t[i][j]=1;}
+            else{t[i][j]=0;}
+        }
+    }
+
+    while(p>0){
+        if(p%2==1){
+            t=sq_mat_mul(t,mat,n);
+        }
+
+        mat=sq_mat_mul(mat,mat,n);
+        p/=2;
+
+
+
+    }
+    return t;
+}
+int main(){
+
+
+    long long int **b;
+    b = new long long int *[2];
+    for(int i = 0; i <2; i++){
+        b[i] = new long long int[2];
+    }
+
+    b[0][0]=0;b[0][1]=1;b[1][0]=1;b[1][1]=1;
+
+    int n;
+    cin>>n;
+    b=mat_power(b,n,2);
+    cout<<b[1][1];
+
+return 0;
+}
