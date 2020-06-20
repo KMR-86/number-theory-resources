69894d6eee5da380a65babc75767253611c47011
khandaker mushfiqur rahman <1505086.kmr@ugrad.cse.buet.ac.bd>
2020-06-20 07:55:12 +0600

    2nd problem understood and solved

--- contests/Codeforces 1368/b.cpp
+++ contests/Codeforces 1368/b.cpp
@@ -2,35 +2,49 @@
 #define S 100000000
 using namespace std;
 long long int c=0;
-
+int len[10]={0};
+bool is_larger(long long int n){
+    long long int prod=1;
+    for(int i=0;i<10;i++){
+        prod=prod*len[i];
+    }
+    if(prod>=n)return true;
+    return false;
+}
 int main(){
 long long int n;
 cin>>n;
 string s="codeforces";
-n=n-1;
-cout<<"codeforces";
-s="";
-long long int a=1;
-long long int ans=1;
-while(n!=0){
-    if(n%2==1){
-        ans=ans*a;
-        for(int i=0;i<ans;i++){
-            cout<<"s";
-        }
-    }
-    if(a==1){
-        a=2;
-    }
-    else{
-        a=a*a;
-    }
-    n=n/2;
+int l=s.size();
+long long int m=1;
+int d=0;
+while(m<n){
+    m=m*2;
+    d++;
+
 }
-cout<<s;
 
+int j=0;
+for(int i=0;i<l;i++){
+    len[i]=1;
+}
+while(true){
 
+    if(is_larger(n)){
+        break;
+    }
+    len[j]=len[j]+1;
+    j=(j+1)%l;
 
+}
+string ans="";
+for(int i=0;i<l;i++){
+    string t(len[i],s[i]);
+    ans=ans+t;
+}
+cout<<ans;
 return 0;
 }
-
+///421654016
+///ccccccccooooooooddddddddeeeeeeefffffffooooooorrrrrrrccccccceeeeeeesssssss
+///ccccccccooooooooddddddddeeeeeeefffffffooooooorrrrrrrccccccceeeeeeesssssss
